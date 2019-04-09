7448b376767bc896adf92754141f7c40fc4086b6
NEELANSHSETHI <sethineelansh@gmail.com>
2019-04-09 19:16:44 +0530

    Added Contact Fragment

---
+++ app/src/main/java/com/example/myapplication/ContactList.java
@@ -0,0 +1,172 @@
+package com.example.myapplication;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.provider.ContactsContract;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+
+public class ContactList extends Fragment implements
+        LoaderManager.LoaderCallbacks<Cursor>,
+        AdapterView.OnItemClickListener {
+
+    @SuppressLint("InlinedApi")
+    private final static String[] FROM_COLUMNS = {
+            ContactsContract.Contacts.DISPLAY_NAME_PRIMARY
+    };
+    /*
+     * Defines an array that contains resource ids for the layout views
+     * that get the Cursor column contents. The id is pre-defined in
+     * the Android framework, so it is prefaced with "android.R.id"
+     */
+    private final static int[] TO_IDS = {
+            android.R.id.text1
+    };
+    // Define global mutable variables
+    // Define a ListView object
+    ListView contactsList;
+    // Define variables for the contact the user selects
+    // The contact's _ID value
+    long contactId;
+    // The contact's LOOKUP_KEY
+    String contactKey;
+    // A content URI for the selected contact
+    Uri contactUri;
+    // An adapter that binds the result Cursor to the ListView
+    private SimpleCursorAdapter cursorAdapter;
+    public static final int CONTACT_LOADER_ID = 78;
+
+    private static final String[] PROJECTION =
+            {
+                    ContactsContract.Data._ID,
+                    ContactsContract.Data.MIMETYPE,
+                    ContactsContract.Data.DATA1,
+                    ContactsContract.Data.DATA2,
+                    ContactsContract.Data.DATA3,
+                    ContactsContract.Data.DATA4,
+                    ContactsContract.Data.DATA5,
+                    ContactsContract.Data.DATA6,
+                    ContactsContract.Data.DATA7,
+                    ContactsContract.Data.DATA8,
+                    ContactsContract.Data.DATA9,
+                    ContactsContract.Data.DATA10,
+                    ContactsContract.Data.DATA11,
+                    ContactsContract.Data.DATA12,
+                    ContactsContract.Data.DATA13,
+                    ContactsContract.Data.DATA14,
+                    ContactsContract.Data.DATA15
+            };
+
+
+
+    public ContactList() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contact_list, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setupCursorAdapter();
+        getLoaderManager().initLoader(CONTACT_LOADER_ID,new Bundle(),this);
+    }
+
+    private void setupCursorAdapter() {
+        Log.d("qqq","mila");// Gets the ListView from the View list of the parent activity
+
+        // Gets a CursorAdapter
+        cursorAdapter = new SimpleCursorAdapter(getActivity(),R.layout.contacts_list_item,null,FROM_COLUMNS, TO_IDS,0);
+        // Sets the adapter for the ListView
+        contactsList = getActivity().findViewById(R.id.contacts_list_view);
+        contactsList.setAdapter(cursorAdapter);
+        contactsList.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, @Nullable Bundle bundle) {
+
+        String[] projectionFields = new String[] { ContactsContract.Contacts._ID,
+                ContactsContract.Contacts.DISPLAY_NAME,
+                ContactsContract.Contacts.PHOTO_URI };
+
+        CursorLoader loader = new CursorLoader(
+                this.getActivity(),
+                ContactsContract.Contacts.CONTENT_URI,
+                projectionFields,
+                null,
+                null,
+                ContactsContract.Contacts.DISPLAY_NAME);
+        return loader;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
+
+        cursorAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+        cursorAdapter.swapCursor(null);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    /*    Cursor cursor = parent.getAdapter().getCursor();
+        // Move to the selected contact
+        cursor.moveToPosition(position);
+        // Get the _ID value
+        contactId = cursor.getLong(CONTACT_ID_INDEX);
+        // Get the selected LOOKUP KEY
+        contactKey = cursor.getString(CONTACT_KEY_INDEX);
+        // Create the contact's content Uri
+        contactUri = ContactsContract.Contacts.getLookupUri(contactId, mContactKey);
+        /*
+         * You can use contactUri as the content URI for retrieving
+         * the details for a contact.
+         */
+
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
